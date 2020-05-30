059a0791c1b0e45688573f81711aabb458ab2367
Armin Imamovic <armin.imamovic@commercecare.de>
2020-05-30 12:15:53 +0200

    Implemented react-router for login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Landing from './components/Landing';
+import Landing from './components/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 const App = () => (
-  <Fragment>
-      <Navbar />
-      <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+        <Navbar />
+        <section className="container">
+          <Switch>  
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
+        <Route exact path="/" component={ Landing } />
+    </Fragment>
+  </Router>
 )
     
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
         <h1>
-            <a href="index.html"><i className="fas fa-code"></i>Workout Tracker</a>
+            <Link to="/">
+                <i className="fas fa-code"></i>Workout Tracker
+            </Link>
         </h1>
+        <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+        </ul>
         </nav>
     )
 }
